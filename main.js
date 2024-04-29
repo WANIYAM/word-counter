c3b2838c12ba30c2b06fec348025fb5a849e4657
WANIYAM <waniya.mustafa75@gmail.com>
2024-04-29 22:05:50 +0500

    word counter

---
+++ main.js
@@ -0,0 +1,84 @@
+#! /usr/bin/env node
+// Importing modules
+import inquirer from "inquirer";
+import chalk from "chalk";
+import Table from 'cli-table';
+// welcome note
+console.log(chalk.yellow("\t\t\t\t\tWELCOME TO WORD COUNTER\t\t\t\t\t"));
+// making function 
+//Function for main menu 
+async function mainMenu() {
+    // TAKING USER INPUT
+    let main = await inquirer.prompt([
+        {
+            name: "menu",
+            type: "list",
+            choices: ["word counter", "exit"]
+        }
+    ]);
+    if (main.menu === "word counter") {
+        let userText = await inquirer.prompt([
+            {
+                name: "text",
+                type: "input",
+                message: chalk.cyan.bold("Enter you text")
+            }
+        ]);
+        // Create a new CLI table
+        let table = new Table();
+        table.push({ "Word Count": countWords(userText.text) }, { "Character Count (with whitespace)": countCharactersWithWhitespace(userText.text) }, { "Character Count (without whitespace)": countCharactersWithoutWhitespace(userText.text) }, { "Unique Words Count": countUnique(userText.text).toString() } // Convert number to string 
+        );
+        // Display the table
+        console.log(table.toString());
+    }
+    else {
+        console.log(chalk.yellow("EXITING......\n \t\t\t\t\tTHANK YOU !!! HAVE A NICE DAY"));
+    }
+}
+;
+// Function to count words
+function countWords(text) {
+    if (!text) {
+        return 0; // or handle the case based on your requirements
+    }
+    let words = text.trim().split(/\s+/);
+    return words.length;
+}
+;
+// Function to count characters including whitespace
+function countCharactersWithWhitespace(text) {
+    return text.length;
+}
+;
+// Function to count characters excluding whitespace
+function countCharactersWithoutWhitespace(text) {
+    return text.replace(/\s/g, "").length; // \/s/g : is used for searching and replacing whitespace characters in a string
+}
+;
+// Function to count unique words
+function countUnique(text) {
+    const words = new Set(text.trim().toLowerCase().split(/\s+/));
+    return words.size;
+}
+;
+// Function for recurrence 
+// Function for recurrence 
+async function repeat() {
+    let question = await inquirer.prompt([
+        {
+            name: "answer",
+            type: "list",
+            message: "Do you want to restart?",
+            choices: ["yes", "no"] // Removed unnecessary comma
+        }
+    ]);
+    if (question.answer === "yes") {
+        await mainMenu(); // Added "await" here
+        await repeat(); // Added "await" here
+    }
+    else {
+        console.log(chalk.yellow("\t\t\t\t\tTHANK YOU FOR USING WORD COUNTER\t\t\t\t\t"));
+    }
+}
+await mainMenu();
+await repeat();
